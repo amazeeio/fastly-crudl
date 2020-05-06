c796ccc9ceeb4de9b5714b388b704ac46f0441ff
Rodrigo Aguilera <rodrigo.aguilera@amazee.com>
2020-05-06 16:56:31 +0200

    Fix data certificates access to properties.

--- src/Fastly/Types/FastlyCertificate.php
+++ src/Fastly/Types/FastlyCertificate.php
@@ -15,62 +15,61 @@ class FastlyCertificate implements FastlyCertificateInterface
 
     public function getId()
     {
-        return $this->data->id;
+        return $this->data['id'];
     }
 
     public function getType()
     {
-        return $this->data->type;
+        return $this->data['type'];
     }
 
     public function getCreatedAt()
     {
-        return $this->data->attributes->created_at;
+        return $this->data['attributes']['created_at'];
     }
 
     public function getIssuedTo()
     {
-        return $this->data->attributes->issued_to;
+        return $this->data['attributes']['issued_to'];
     }
 
     public function getIssuer()
     {
-        return $this->data->attributes->issuer;
+        return $this->data['attributes']['issuer'];
     }
 
     public function getName()
     {
-        return $this->data->attributes->name;
+        return $this->data['attributes']['name'];
     }
 
     public function getNotAfter()
     {
-        return $this->data->attributes->not_after;
+        return $this->data['attributes']['not_after'];
     }
 
     public function getNotBefore()
     {
-        return $this->data->attributes->not_before;
+        return $this->data['attributes']['not_before'];
     }
 
     public function getReplace()
     {
-        return $this->data->attributes->replace;
+        return $this->data['attributes']['replace'];
     }
 
     public function getSerialNumber()
     {
-        return $this->data->attributes->serial_number;
+        return $this->data['attributes']['serial_number'];
     }
 
     public function getSignatureAlgorithm()
     {
-        return $this->data->attributes->signature_algorithm;
+        return $this->data['attributes']['signature_algorithm'];
     }
 
     public function getUpdatedAt()
     {
-        return $this->data->attributes->updated_at;
+        return $this->data['attributes']['updated_at'];
     }
-
 }
